f3261bf71db5343f3d75c8974c4f28d367d7fb2e
eager-loop-4095 <jingchenjc2019@gmail.com>
2019-01-20 19:37:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return "I love you, too.";
-  } else if (string)
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } else if (string === string.toLowerCase()) {
+    return "I can\'t hear you!"
+  }
